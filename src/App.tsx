4247177fa66d18660beb910394f164e8a1e86b40
Arefevpro <arefevmsk@gmail.com>
2024-04-19 09:19:48 +0300

    ./

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Body } from "./site/Body";
 import { Footer } from "./site/Footer";
 import { Header } from "./site/Header";
 import { Button } from "./site/Button";
+import { log } from "console";
 
 function App() {
 const Button1Foo = (subscriber: string, age: number, address: string) => {
@@ -18,6 +19,18 @@ const Button3Foo = () => {
   console.log('im stupid button')
 }
 
+// let a = 1;
+let [a, setA] = useState (0)
+
+const onClickHandler = () => {
+  setA (++a);
+  console.log(a)
+}
+
+const reset = () => {
+  setA (0);
+}
+
   return (
     <>
       <Header titleHeader={"Header"} />
@@ -26,6 +39,9 @@ const Button3Foo = () => {
       <Button name={"MyYoutobeChanel-1"} callBack={()=>Button1Foo('im MalGanis', 33, 'Live in Moscow')}/>
       <Button name={"MyYoutobeChanel-2"} callBack={()=>Button2Foo('im Trall')}/>
       <Button name={"stupid button"} callBack={Button3Foo}/>
+      <h1>{a}</h1>
+      <button onClick={onClickHandler}>number</button>
+      <button onClick={reset}>0</button>
     </>
   );
 }
